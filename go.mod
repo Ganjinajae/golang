555f99cd3ca9c85376256284b51253acefe2bbd9
지영진 <youngjin.jee@nhn.com>
2023-02-23 18:05:11 +0900

    go lang 실습

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ganjinajae/golang/packages/printeo
+
+go 1.19
---
+++ printeo.go
@@ -0,0 +1,7 @@
+package printeo
+
+import "fmt"
+func PrintlnMyView(value string) {
+	fmt.Println("my printeo start")
+	fmt.Println("value is " + value)
+}
